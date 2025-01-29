d3a368c6bfd46ff313b9e9f16df5eac3db515e84
Nadiia1703 <onadi@ukr.net>
2025-01-29 23:50:40 +0200

    create new branch

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function sayHello(name, surname, someWord) {
         console.log (`Hello ${name} ${surname} ${someWord}`);
 } 
 sayHello('Nadin', 'Yatsenko', 'Welcome!');
+sayHello('Robot', 'Dreams', 'Welcome!');
