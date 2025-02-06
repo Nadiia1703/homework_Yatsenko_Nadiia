7a63cf48401e20649b58d4b31ac21b995b5f1cc6
Nadiia1703 <onadi@ukr.net>
2025-02-07 00:44:34 +0200

    lesson5_

--- lesson5/src/arrow_fuctions.js
+++ lesson5/src/arrow_fuctions.js
@@ -2,7 +2,7 @@ const sumArr = (arr) => arr.reduce((sum, current) => sum + current, 0);
 
 const sumArrWithoutString =(arr ) => arr.reduce((sum, current) => {
     return sum + (typeof current === 'number' ? current : Number(current) );
-}, 0);
+});
 
 
 const a = [1,3,6,'7'];
