e278d9d4ae4661134bd87303747477babe5942a5
Nadiia1703 <onadi@ukr.net>
2025-02-19 11:55:58 +0200

    fix of remark

--- lesson7/src/arrow-functons.ts
+++ lesson7/src/arrow-functons.ts
@@ -1,13 +1,13 @@
-const sunNum = (arr: (number | string)[]): undefined =>  {
+const sunNum = (arr: (number | string)[]): void =>  {
     console.log('Sum number ', arr.reduce((s: number, c: number | string) => s + (typeof c === 'number' ? c : Number(c)), 0));
 };
 
-const concatStrings = (arr: (number | string)[]) : undefined =>{
+const concatStrings = (arr: (number | string)[]) : void =>{
     console.log ('Concat text ', arr.reduce((s: string, c: number | string) => s + (typeof c === 'string' ? c : ('')), ' '));
 };
 
-const a: (number | string)[] = [1, 3, 6, '7'];
-const b: (number | string)[] = [0, '1', '3', 'test', 9];
+const a = [1, 3, 6, '7'];
+const b = [0, '1', '3', 'test', 9];
 
 sunNum(a);
 sunNum(b);
--- lesson7/src/functions.ts
+++ lesson7/src/functions.ts
@@ -1,11 +1,8 @@
 function sumArr(arr: number[]): number{
-    if (arr.length !== 0 )   {
-        return arr.reduce((s, c) => s + (typeof c === 'number' ? c : 0), 0);
-    }
-    return 0;
+    return arr.reduce((s, c) => s + (typeof c === 'number' ? c : 0), 0);
 }
 
-function sumString(arr: (string | number) []): undefined{
+function sumString(arr: (string | number) []): void{
     console.log('text:', arr.reduce((s, c) => s + (typeof c === 'string' ? c : ''), ''));
 }
 
